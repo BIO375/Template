810b789cd5fbd8ed4ba342a9460cd9d59bf559ab
Sarah Lee <scrislee@gmail.com>
2019-09-02 22:47:44 -0400

    Mostly done, need to add var and stdev example and instructions

--- source-code/BIO375_Lab2.R
+++ source-code/BIO375_Lab2.R
@@ -25,4 +25,58 @@ x <- 3*4
 x
 
 # Read in the data file
-read_csv("datasets/demos/2019-08-28_class-data.csv", )
+data<-read_csv("datasets/demos/2019-09-02_class-data.csv",col_names = TRUE,
+               col_types = cols(
+  name = col_character() )
+)
+
+# Generate summary statistics
+summary(data)
+
+# Plot height as a histogram
+ggplot(data)+
+  geom_histogram(aes(height), binwidth = 10)
+
+# Plot right arm length as a histogram
+ggplot(data)+
+  geom_histogram(aes(right_arm), binwidth = 5)
+
+# Note that you must have the column name exactly correct.  Try, for example,
+# the following code
+ggplot(data)+
+  geom_histogram(aes(right arm), binwidth = 10)
+
+# Look at the help file for geom_boxpolot
+help("geom_boxplot")
+
+# Plot height as a boxplot
+ggplot(data)+
+  geom_boxplot(aes(x = "", y = height), notch = TRUE, varwidth = TRUE)
+
+# Plot height as a three boxplots, one for each hair color
+ggplot(data)+
+  geom_boxplot(aes(x = hair_color, y = height), notch = TRUE, varwidth = TRUE)
+
+### Assignment
+
+# Load a larger dataset, lovett.csv, and assign the name data2.
+# Dataset found at datasets/quinn/chpt2/lovett.csv
+# Enter your code below
+
+
+
+# Calculate summary statistics for SO4 and SO4MOD
+# Enter your code below
+
+
+
+# Calculate variance and standard deviation for SO4 and SO4MOD
+
+# Plot histograms of DOC and Modified DOC with appropriate bin sizes
+# To generate a Modified DOC histogram, you will need to execute the following code
+
+data3 -> data2 %>%
+  filter(STREAM != Santa_Cruz)
+
+
+
