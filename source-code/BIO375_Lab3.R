ac789b961eff6a0e158c2de929e362e4e9981749
Sarah Lee <scrislee@gmail.com>
2019-09-09 23:32:27 -0400

    Lab 3 assignment added

--- source-code/BIO375_Lab3.R
+++ source-code/BIO375_Lab3.R
@@ -19,6 +19,9 @@ ward_data<-read_csv("datasets/quinn/chpt3/ward.csv", col_names = TRUE)
 library(readr)
 ward <- read_csv("datasets/quinn/chpt3/ward.csv")
 
+# Read in compensation data file
+compensation<-read_csv("datasets/demos/compensation.csv")
+
 # names() tells you the names assigned to each column, generally variable
 # names
 names(ward)
@@ -32,21 +35,95 @@ dim(ward)
 # str() returns the structure of the dataset
 str(ward)
 
-# Enter code from R for Data Science, Chapter 3
-# https://r4ds.had.co.nz/data-visualisation.html
+# Calculate summary statistics about groups.  I give the general form below
+# in comments
 
+# <new_object_name> <- <data> %>%
+# group_by(<grouping_variable>) %>%
+# summarise(
+# mean_resp = mean(<response_variable_name>),
+# median_resp = median(<response_variable_name>),
+# IQR_resp = IQR(<response_variable_name>),
+# sd_resp = sd(<response_variable_name>),
+# var_resp = var(<response_variable_name>)
+# )
 
+summ_eggs <- ward %>%
+group_by(ZONE) %>% 
+  summarise(mean_eggs = mean(EGGS),
+            median_eggs = median(EGGS),
+            IQR_eggs = IQR(EGGS),
+            sd_eggs = sd(EGGS),
+            var_eggs = var(EGGS))
 
+View(summ_eggs)
 
-
-
-# mutate() adds new variables while preserving existing ones.  
+# mutate() adds new variables while preserving existing ones.  General form:
+# <dataset_name> <- mutate(<dataset_name>, <transform_variable_name> =
+# <mathematical_function>(<variable_name>))
 ward<-mutate(ward, squareroot_eggs = sqrt(EGGS))
 
-# Compare the histograms and boxplots of EGGS and squareroot_eggs# Plot height as a histogram
+compensation<-mutate(compensation, log(Root))
+
+# R for Data Science, Chapter 3
+# https://r4ds.had.co.nz/data-visualisation.html
+# Enter your code here
+
+
+
+
+
+
+
+
+
+
+# Compare the histograms and boxplots of EGGS and squareroot_eggs
 ggplot(ward) +
   geom_histogram(aes(EGGS), binwidth = 2)+
   facet_wrap(~ZONE)
 ggplot(ward) +
   geom_histogram(aes(squareroot_eggs), binwidth = 0.5)+
   facet_wrap(~ZONE)
+
+ggplot(ward)+
+  geom_boxplot(aes(x = ZONE, y = EGGS), notch = TRUE, varwidth = TRUE)
+ggplot(ward)+
+  geom_boxplot(aes(x = ZONE, y = squareroot_eggs), notch = TRUE, varwidth = TRUE)
+
+### Assignment
+
+# Load the sanchez.csv file
+# Enter your code here
+
+
+
+
+
+# Calculate summary statistics
+# Enter your code here
+
+
+
+
+
+
+# Add a new column of log(y+1) transformed beetle densities to the sanchez dataset
+# Enter your code here
+
+
+
+
+
+# Generate histograms of beetle density by colony type before and after data 
+# transformation
+# Enter your code here
+
+
+
+
+
+# Plot boxplots of beetle density by colony type before and after data 
+# transformation
+# Enter your code here
+
