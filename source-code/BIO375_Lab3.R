f054364e573c1b0415506d1128c8dc17f2ea5227
Sarah Lee <scrislee@gmail.com>
2019-09-09 20:27:25 -0400

    Midway through Lab3

--- source-code/BIO375_Lab3.R
+++ source-code/BIO375_Lab3.R
@@ -5,9 +5,42 @@ rm(list = ls())
 # Verify working directory, should be ~/Documents/Analyses/lastname_first
 getwd()
 
-# Note that this loads the following packages: 
+# Load tidyverse
 library("tidyverse")
 # Check for updates
 tidyverse_update()
 
+# Read in data file
+ward_data<-read_csv("datasets/quinn/chpt3/ward.csv", col_names = TRUE)
 
+# Pasted from Import Dataset Tool
+# Note that for us, library(readr) is redundant because we loaded it with
+# all the other tidyverse packages earlier
+library(readr)
+ward <- read_csv("datasets/quinn/chpt3/ward.csv")
+
+# names() tells you the names assigned to each column, generally variable
+# names
+names(ward)
+
+# head() gives you the first six rows of a dataset
+head(ward)
+
+# dim() gives you the dimensions of your dataset
+dim(ward)
+
+# str() returns the structure of the dataset
+str(ward)
+
+# Enter code from R for Data Science, Chapter 3
+# https://r4ds.had.co.nz/data-visualisation.html
+
+
+
+
+
+
+# mutate() adds new variables while preserving existing ones.  
+ward<-mutate(ward, squareroot_eggs = sqrt(EGGS))
+
+# Compare the histograms and boxplots of EGGS and squareroot_eggs
