638089333a672f0b2c4c9cff2d4827e5f7c810e8
Sarah Lee <scrislee@gmail.com>
2019-10-07 11:17:02 -0400

    ABD 12-18 code

--- source-code/BIO375_ABD_practice-probs.R
+++ source-code/BIO375_ABD_practice-probs.R
@@ -15,5 +15,34 @@ tidyverse_update()
 install.packages("DescTools")
 library("DescTools")
 
+# To simplify summary statistics, install and load the package summarytools
+install.packages("summarytools")
+library("summarytools")
+
 ### Problem 12-18 ######
 eyespan <- read_csv("datasets/abd/chapter12/chap12q18StalkieEyespan.csv")
+
+# a.
+# Do the data meet the assumption of normality?  Check with plots.
+ggplot(eyespan) +
+  geom_histogram(aes(eyeSpan), binwidth = .1)+
+  facet_wrap(~food)
+ggplot(eyespan) +
+  geom_boxplot(aes(x = food, y = eyeSpan))
+ggplot(eyespan)+
+  geom_qq(aes(sample = eyeSpan, color = food))
+# Yes, the data are symmetrical (top & bottom whisker equal for each box) 
+# and mostly linear q-q plots.
+
+# Do the data meet the assumption of homogeneous variance?  Check ratio.
+(eyespan_ratio <- 0.0812/0.00558) 
+# No, the groups do not have equal variance.  Use Welch's t-test.
+
+# b.
+t.test(eyeSpan ~ food, data = eyespan, alternative = "two.sided",
+       conf.level = 0.95)
+# Eye spans in male stalk-eyed flies raised on a corn diet were larger 
+# than eye spans of flies raised on a cotton diet (Welch's t-test,
+# two-sided: t = 8.35, df = 26.57, P < 0.0001).
+
+### Problem 12-20 ######
